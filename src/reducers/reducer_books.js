0551c83b49553d02cb34d0ee1c0a052825549818
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-17 15:02:29 +0200

    simple reducer for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Dummy Book 1'},
+        { title: 'Dummy Book 2'},
+        { title: 'Dummy Book 3'},
+        { title: 'Dummy Book 4'}
+    ]
+}
