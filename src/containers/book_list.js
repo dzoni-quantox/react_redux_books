2b8a1c8f1a3959b5b7f717f56b1eb56082ff0f51
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-23 15:58:18 +0200

    added selecting a book and showing details

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from "../actions/index";
+import {bindActionCreators} from "redux";
 
 class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item"
+                >
+                    {book.title}
+                </li>
             )
         })
     }
@@ -20,9 +28,20 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
+    // what is returned will show up as props
+    // in BookList
     return {
         books: state.books
     }
 }
 
-export default connect(mapStateToProps)(BookList);
+// what is returned will show up as props
+// in BookList
+function mapDispatchToProps(dispatch) {
+    // whenever selectBook is called
+    // the result should be passed to all reducers
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'Dummy Book 1'},
-        { title: 'Dummy Book 2'},
-        { title: 'Dummy Book 3'},
-        { title: 'Dummy Book 4'}
+        { title: 'Dummy Book 1', pages: 101},
+        { title: 'Dummy Book 2', pages: 102},
+        { title: 'Dummy Book 3', pages: 103},
+        { title: 'Dummy Book 4', pages: 104}
     ]
 }
